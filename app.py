c2d9cb95cb16b67859fc3f26726efc9cc670f8f2
NinaMiyazaki <24215@burnside.school.nz>
2026-05-06 10:24:30 +1200

    created another function

--- app.py
+++ app.py
@@ -21,14 +21,36 @@ def print_all_cars():
     # loop finished here
     db.close()
 
+def print_cars_with_speed():
+    speed = input("What speed? ")
+    '''print all cars with speed'''
+    db = sqlite3.connect("cars.db")
+    cursor = db.cursor()
+    sql = "SELECT * FROM car WHERE top_speed > ?;"
+    cursor.execute(sql, (speed,))
+    results = cursor.fetchall()
+    print("car_name                      top_speed")
+    # loop through all the results
+    for car in results:
+        print(f"{car[1]:<30}{car[2]:<4}")
+    # loop finished here
+    db.close()
 
 
 # main code
 while True:
-    user_input = input("\nWhat would you like to do.\n1. Print all cars\n2. Exit\n")
+    user_input = input(
+    """
+    What would you like to do?
+    1. Print all cars
+    2. Print cars with specific speed
+    3. Exit
+    """)
     if user_input == "1":
         print_all_cars()
     elif user_input == "2":
+        print_cars_with_speed()
+    elif user_input == "3":
         break
     else:
         print("That was not an option\n")
